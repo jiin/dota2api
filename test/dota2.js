7c42d3599de2222628a092589c01d08cd5ba6c45
Nyu <admin@queeply.com>
2013-07-24 20:55:08 +0200

    first commit

---
+++ test/dota2.js
@@ -0,0 +1,18 @@
+var expect   = require('expect.js'),
+	Dota2Api = require('../lib/dota2api.js'),
+	dota     = new Dota2Api('830F9D2CE8476B24E02F386E2694972C');
+
+
+describe('test internals methods', function() {
+	it('should be ok', function() {
+		expect(dota).to.be.ok();
+	});
+
+	it('should return string', function() {
+		expect(dota.param(['foo', 'bar'], ['1', '2'])).to.be.a('string');
+	});
+
+	it('should return specified value', function() {
+		expect(dota.param(['foo', 'bar'], ['1', '2'])).to.eql('?foo=1&bar=2');
+	});
+});
---
+++ test/index.js
@@ -0,0 +1,1 @@
+require('./dota2.js');
